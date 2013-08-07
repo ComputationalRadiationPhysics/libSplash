203e13440acc58b6ec8d6453563c14e882498f8f
Felix Schmitt <felix.schmitt@zih.tu-dresden.de>
2013-08-07 15:24:26 +0200

    Added missing GPL snippet

--- src/include/DCParallelDataSet.hpp
+++ src/include/DCParallelDataSet.hpp
@@ -1,8 +1,22 @@
-/* 
- * File:   DCParallelDataSet.hpp
- * Author: felix
+/**
+ * Copyright 2013 Felix Schmitt
  *
- * Created on 5. August 2013, 14:02
+ * This file is part of libSplash. 
+ * 
+ * libSplash is free software: you can redistribute it and/or modify 
+ * it under the terms of of either the GNU General Public License or 
+ * the GNU Lesser General Public License as published by 
+ * the Free Software Foundation, either version 3 of the License, or 
+ * (at your option) any later version. 
+ * libSplash is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+ * GNU General Public License and the GNU Lesser General Public License 
+ * for more details. 
+ * 
+ * You should have received a copy of the GNU General Public License 
+ * and the GNU Lesser General Public License along with libSplash. 
+ * If not, see <http://www.gnu.org/licenses/>. 
  */
 
 #ifndef DCPARALLELDATASET_HPP
