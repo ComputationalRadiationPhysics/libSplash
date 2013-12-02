c5ae79e91041ede762fbb9a7a1a5ba1e8f1246c6
Felix Schmitt <felix.schmitt@zih.tu-dresden.de>
2013-12-02 09:19:39 +0100

    Add macros to create generic basetypes
     - remove old type files

--- src/include/basetypes/basetypes_atomic.hpp
+++ src/include/basetypes/basetypes_atomic.hpp
@@ -18,38 +18,45 @@
  * and the GNU Lesser General Public License along with libSplash. 
  * If not, see <http://www.gnu.org/licenses/>. 
  */ 
- 
 
+#ifndef BASETYPES_ATOMIC_HPP
+#define	BASETYPES_ATOMIC_HPP
 
-#ifndef COLTYPEFLOAT3ARRAY_H
-#define	COLTYPEFLOAT3ARRAY_H
+#include <stdint.h>
 
 #include "CollectionType.hpp"
 
 namespace splash
 {
-    class ColTypeFloat3Array : public CollectionType
-    {
-    public:
-
-        ColTypeFloat3Array()
-        {
-            const hsize_t dim[] = {3};
-            this->type = H5Tarray_create(H5T_FLOAT, 1, dim);
-        }
-
-        ~ColTypeFloat3Array()
-        {
-            H5Tclose(this->type);
-        }
-
-        size_t getSize() const
-        {
-            return sizeof (float) * 3;
-        }
+    
+#define TYPE_ATOMIC(_name, _h5_type, _real_type) \
+    class ColType##_name : public CollectionType \
+    { \
+    public: \
+ \
+        ColType##_name() \
+        { type = _h5_type; } \
+ \
+        size_t getSize() const \
+        { return sizeof (_real_type); } \
     };
+    
+TYPE_ATOMIC(Float, H5T_NATIVE_FLOAT, float);
+TYPE_ATOMIC(Double, H5T_NATIVE_DOUBLE, double);
 
+TYPE_ATOMIC(Int, H5T_NATIVE_INT, int);
+
+TYPE_ATOMIC(UInt8, H5T_INTEL_U8, uint8_t);
+TYPE_ATOMIC(UInt16, H5T_INTEL_U16, uint16_t);
+TYPE_ATOMIC(UInt32, H5T_INTEL_U32, uint32_t);
+TYPE_ATOMIC(UInt64, H5T_INTEL_U64, uint64_t);
+
+TYPE_ATOMIC(Int8, H5T_INTEL_I8, int8_t);
+TYPE_ATOMIC(Int16, H5T_INTEL_I16, int16_t);
+TYPE_ATOMIC(Int32, H5T_INTEL_I32, int32_t);
+TYPE_ATOMIC(Int64, H5T_INTEL_I64, int64_t);
+    
 }
 
-#endif	/* COLTYPEFLOAT3ARRAY_H */
+#endif	/* BASETYPES_ATOMIC_HPP */
 
