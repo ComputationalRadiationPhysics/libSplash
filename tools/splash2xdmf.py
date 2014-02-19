5f454759701454b3970d8c278b92c5cebf960e3d
Felix Schmitt <felix.schmitt@zih.tu-dresden.de>
2014-02-19 12:35:29 +0100

    Support time-series detection and output filename

--- tools/splash2xdmf.py
+++ tools/splash2xdmf.py
@@ -25,6 +25,7 @@
 import sys
 import h5py
 import os.path
+import glob
 import argparse
 from xml.dom.minidom import Document
 
@@ -37,7 +38,7 @@ verbosity_level = 0
 doc = Document()
 grids = dict()
 
-# helper functions
+# String functions
 
 def log(msg, linebreak=True, verbLevel=1):
     if verbosity_level >= verbLevel:
@@ -46,23 +47,28 @@ def log(msg, linebreak=True, verbLevel=1):
         else:
             print msg,
 
+def get_common_filename(filename):
+    index1 = filename.rfind(".h5")
+    if index1 == -1:
+        return filename
+    else:
+        index2 = filename[0:index1].rfind("_")
+        if index2 == -1:
+            return filename
+        else:
+            return filename[0:index2]
+        
 
-# XDMF output functions
+# strip /data/<timestep> from dset_name
+def get_attr_name(dset_name):
+    index = dset_name.find("/", len("/data/"))
+    if index == -1:
+        return dset_name
+    else:
+        return dset_name[(index+1):]
 
-# get the number of dimensions of an hdf5 dataset
-def get_ndims(dset):
-    return len(dset.dims)
 
-# get dimensions as string for an hdf5 dataset
-def get_dims(dset):
-    ndims = get_ndims(dset)
-    dims = ""
-    for i in range(ndims):
-        dims += "{}".format(dset.shape[i])
-        if i < ndims - 1:
-            dims += " "
-
-    return dims
+# XDMF/XML output functions
 
 # return an existing xml grid node for dims or create a new one
 # and insert it into the map of grid nodes
@@ -110,14 +116,6 @@ def get_create_grid_node(dims, ndims):
         grids[dims] = new_grid
         return new_grid
 
-# strip /data/<timestep> from dset_name
-def get_attr_name(dset_name):
-    index = dset_name.find("/", len("/data/"))
-    if index == -1:
-        return dset_name
-    else:
-        return dset_name[(index+1):]
-    
 
 # create an xdmf grid attribute for an hdf5 grid-type dataset
 def create_xdmf_grid_attribute(dset, level, h5filename):
@@ -143,25 +141,30 @@ def create_xdmf_grid_attribute(dset, level, h5filename):
     
     grid.appendChild(attribute)
     
-    
-def create_xdmf_header(xdmf):
-    domain = doc.createElement("Domain")
-    grid = doc.createElement("Grid")
-    
-    grid.setAttribute("Name", "Box")
-    grid.setAttribute("GridType", "Collection")
-    
-    domain.appendChild(grid)
-    xdmf.appendChild(domain)
-    return grid
+
+def create_timestep_node(time):
+    time_node = doc.createElement("Time")
+    time_node.setAttribute("TimeType", "Single")
+    time_node.setAttribute("Value", "{}".format(time))
+    return time_node
 
 
-def finalize_xdmf(xdmf):
-    for grid in grids.values():
-        xdmf.appendChild(grid)
+# HDF5 functions
 
+# get the number of dimensions of an hdf5 dataset
+def get_ndims(dset):
+    return len(dset.dims)
 
-# HDF5 parsing functions
+# get dimensions as string for an hdf5 dataset
+def get_dims(dset):
+    ndims = get_ndims(dset)
+    dims = ""
+    for i in range(ndims):
+        dims += "{}".format(dset.shape[i])
+        if i < ndims - 1:
+            dims += " "
+
+    return dims
 
 def eval_class_type(classType, dset, level, h5filename):
     if classType == SPLASH_CLASS_TYPE_GRID:
@@ -190,17 +193,111 @@ def parse_hdf5_recursive(h5Group, h5filename, level):
         else:
             log("(D):", False)
             print_dataset(h5Group[g], level, h5filename)
+    
+
+def get_timestep_for_group(dataGroup):
+    groups = dataGroup.keys()
+    if len(groups) != 1:
+        print("Error: data group is expected to have a single timestep child group")
+        return -1
+    
+    objClass = dataGroup.get(groups[0], None, True)
+    if objClass != h5py.Group:
+        print("Error: data group child is expected to be of object class Group")
+        return -1
+        
+    index = groups[0].rfind("/")
+    return groups[0][(index+1):]
+    
+
+def create_xdmf_for_splash_file(base_node, splashFilename):
+    if not os.path.isfile(splashFilename):
+        print "Error: '{}' does not exist.".format(splashFilename)
+        sys.exit(1)
+        
+    # open libSplash file
+    h5file = h5py.File(splashFilename, "r")
+    log("Opened libSplash file '{}'".format(h5file.filename))
+    
+    # find /data group
+    dataGroup = h5file.get("data")
+    if dataGroup == None:
+        print "Error: Could not find root data group in '{}'.".format(splashFilename)
+        h5file.close()
+        return False
+    
+    time_step = get_timestep_for_group(dataGroup)
+    
+    parse_hdf5_recursive(dataGroup, splashFilename, 1)
+    # append all collected grids to current xml base node
+    index = 0
+    for grid in grids.values():
+        grid.setAttribute("Name", "Grid_{}_{}".format(time_step, index))
+        grid.appendChild(create_timestep_node(time_step))
+        base_node.appendChild(grid)
+        index += 1
+ 
+    grids.clear()
+    
+    # close libSplash file
+    h5file.close()
+    return True
 
 
+# program functions
+
+# creates the xdmf xml structure in the current document for all libSplash
+# files in splash_files_list
+def create_xdmf_xml(splash_files_list):
+    time_series = (len(splash_files_list) > 1)
+    
+    # setup xml structure
+    xdmf_root = doc.createElement("Xdmf")
+    domain = doc.createElement("Domain")
+    base_node = domain
+    
+    if time_series:
+        main_grid = doc.createElement("Grid")
+        main_grid.setAttribute("Name", "Grids")
+        main_grid.setAttribute("GridType", "Collection")
+        main_grid.setAttribute("CollectionType", "Temporal")
+        base_node = main_grid
+    
+    for current_file in splash_files_list:
+        # parse this splash file and append to current xml base node
+        create_xdmf_for_splash_file(base_node, current_file)
+
+    # finalize xml structure
+    if time_series:
+        domain.appendChild(main_grid)
+
+    xdmf_root.appendChild(domain)
+    doc.appendChild(xdmf_root)
+    return xdmf_root
+
+
+def write_xml_to_file(filename):
+    # write xml to xdmf file
+    xdmf_file = open(filename, "w")
+    xdmf_file.write(doc.toprettyxml())
+    xdmf_file.close()
+    log("Created XDMF file '{}'".format(filename), True, 0)
+    
+    
 def get_args_parser():
     parser = argparse.ArgumentParser(description="Create a XDMF meta description file from a libSplash HDF5 file.")
 
-    parser.add_argument("splashfile", metavar="<libSplash file>",
+    parser.add_argument("splashfile", metavar="<filename>",
         help="libSplash HDF5 file with domain information")
         
-    parser.add_argument("-v", help="Produce verbose output", action="store_true")
+    parser.add_argument("-o", metavar="<filename>", help="Name of output XDMF file (default: append '.xmf')")
+        
+    parser.add_argument("-v", "--verbose", help="Produce verbose output", action="store_true")
+    
+    parser.add_argument("-t", "--time", help="Aggregate information over a time-series of libSplash data", action="store_true")
         
     return parser
+
 
 # main
 
@@ -213,40 +310,25 @@ def main():
 
     # apply arguments
     splashFilename = args.splashfile
-    if args.v:
+    time_series = args.time
+    if args.verbose:
         verbosity_level = 100
-    
-    if not os.path.isfile(splashFilename):
-        print "Error: '{}' does not exist.".format(splashFilename)
-        sys.exit(1)
 
-    # open libSplash file
-    h5file = h5py.File(splashFilename, "r")
-    log("Opened libSplash file '{}'".format(h5file.filename))
-
-    # find /data group
-    dataGroup = h5file.get("data")
-    if dataGroup == None:
-        print "Error: Could not find root data group."
+    # create the list of requested splash files
+    splash_files = list()
+    if time_series:
+        splashFilename = get_common_filename(splashFilename)
+        for s_filename in glob.glob("{}_*.h5".format(splashFilename)):
+            splash_files.append(s_filename)
     else:
-        xdmf_filename = "{}.xmf".format(splashFilename)
-        xdmf_file = open(xdmf_filename, "w")
-
-        xdmf_root = doc.createElement("Xdmf")
+        splash_files.append(splashFilename)
         
-        grid = create_xdmf_header(xdmf_root)
-        parse_hdf5_recursive(dataGroup, splashFilename, 1)
-        finalize_xdmf(grid)
-        
-        doc.appendChild(xdmf_root)
-
-        xdmf_file.write(doc.toprettyxml())
-        xdmf_file.close()
-        log("Created XDMF file '{}'".format(xdmf_filename), True, 0)
-
-    # close libSplash file
-    h5file.close()
+    create_xdmf_xml(splash_files)
     
+    output_filename = "{}.xmf".format(splashFilename)
+    if args.o:
+        output_filename = args.o
+    write_xml_to_file(output_filename)
 
 if __name__ == "__main__":
     main()
