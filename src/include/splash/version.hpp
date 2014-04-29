18cc400e1845432ed1e681bbe451a134dcc1bd9b
Felix Schmitt <felix.schmitt@zih.tu-dresden.de>
2014-04-29 14:27:12 +0200

    Update version numbers and fix user manual

--- src/include/splash/version.hpp
+++ src/include/splash/version.hpp
@@ -24,13 +24,13 @@
 
 /** the splash version reflects the changes in API */
 #define SPLASH_VERSION_MAJOR 1
-#define SPLASH_VERSION_MINOR 1
-#define SPLASH_VERSION_PATCH 1
+#define SPLASH_VERSION_MINOR 2
+#define SPLASH_VERSION_PATCH 0
 
 /** we can always handle files from the same major release
  *  changes in the minor number have to be backwards compatible
  */
-#define SPLASH_FILE_FORMAT_MAJOR 1
+#define SPLASH_FILE_FORMAT_MAJOR 2
 #define SPLASH_FILE_FORMAT_MINOR 0
 
 #endif	/* VERSION_HPP */
